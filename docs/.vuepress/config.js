b42ed2aac5f0b609062edb43551713d61a679a96
YOUR NAME <YOU@EXAMPLE.COM>
2020-06-24 12:11:06 +0800

    上传网站源码到github

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,78 @@
+﻿module.exports = {
+  "title": "一拳超人",
+  "description": "动漫",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitLab",
+            "link": "https://gitlab.com/bobyuan/",
+            "icon": "reco-gitlab"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+    
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      }
+    ],
+	"logo": "/logo_trans.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "谢昊",
+    //"record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": false
+  }
+}
